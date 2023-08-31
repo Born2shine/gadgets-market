0a7317dfd24b271675d5b4107f9c0f150bdef238
Damilola Orija <damilolaomotola20@gmail.com>
2023-08-31 04:36:58 +0100

    cloudinary feature

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     DB_URI: "mongodb://localhost:27017/buynow",
     URL: "http://localhost:3000",
     NEXTAUTH_SECRET: "nextauthbuynowdamilola20",
+    cloud_name: "doezbegbo",
+    api_key: "262169284966112",
+    api_secret: "yM68K3_wi9ET8YDUfiLNcntDDYk",
   },
   images: {
     domains: ["res.cloudinary.com"],
